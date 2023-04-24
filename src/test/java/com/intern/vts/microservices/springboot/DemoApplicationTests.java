83448cdfaad4e578e994077a2033a549c2fdc8e5
tunngungoc <phrafttanyfw@gmail.com>
2023-04-24 09:27:24 +0700

    First Commit

---
+++ src/test/java/com/intern/vts/microservices/springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.vts.microservices.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
